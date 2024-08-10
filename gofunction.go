81e303ec108df4818c88a8371099dd0c3fe14c66
Titiporn Sinpaksa <titiporn.dev@gmail.com>
2024-08-10 17:24:40 +0700

    3ndcommit

--- gofunction.go
+++ gofunction.go
@@ -1,17 +1,5 @@
 package gofunction
 
-import "github.com/PrimForDev/gofunction/operation"
-
-func Addition(a int32, b int32) int32 {
-	var result = a + b
-	return result
-}
-
-func Sub(a int32, b int32) int32 {
-	var result = operation.Subtraction(a, b)
-	return result
-}
-
 func Subtraction(a int32, b int32) int32 {
 	var result = a - b
 	return result
